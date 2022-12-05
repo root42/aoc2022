93f1a0054a1749b1485e551d91df549a6eed7a2d
Arne Schmitz <arne.schmitz@gmx.net>
2022-12-05 21:29:43 +0100

    Day 1 solution

--- src/aoc2022/core.clj
+++ src/aoc2022/core.clj
@@ -1,7 +1,62 @@
 (ns aoc2022.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as string]))
+
+(defn make-integer-vector
+  [string-vector]
+  (apply vector (map #(Integer. %) string-vector))
+  )
+
+(defn read-chunked-integer-input
+  [input-file]
+  (let [input (slurp input-file)
+        chunks (string/split input #"\n\n")]
+    (->> chunks
+         (map string/split-lines)
+         (map make-integer-vector)
+         (apply vector)
+         )))
+
+(defn read-input
+  [input-file]
+  (let [input (slurp input-file)]
+    (apply vector (clojure.string/split-lines input)))
+  )
+
+(defn read-integer-input
+  [input-file]
+  (let [input (slurp input-file)]
+    (apply vector (map #(Integer. %) (clojure.string/split-lines input))))
+  )
+
+(defn read-input-csv
+  [input-file]
+  (let [input (slurp input-file)]
+    (map #(Integer. %) (re-seq #"[^,\n]+" input)))
+  )
+
+;; day 1
+(defn calc-max-calories
+  "Calculates the maximum number of calories in the elf backpacks."
+  [input]
+  (apply max (map #(apply + %) input))
+  )
+
+(defn calc-top-three-calories
+  "Calculates the calories carried by the top three elves."
+  [input]
+  (->> input
+       (map #(apply + %))
+       (sort >)
+       (take 3)
+       (apply +)
+       ))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Advent of Code 2022."
   [& args]
-  (println "Hello, World!"))
+  (let [input (read-chunked-integer-input "resources/input_1.txt")]
+    (println "1.1 max number of calories = " (calc-max-calories input))
+    (println "1.2 top three elves' calories = " (calc-top-three-calories input))
+    )
+  )
